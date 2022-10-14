40c038607edb5f79a4465d112395e7d49891cc9e
Nguyen Van Nam Tamás <nguyen.tamas@arteries.hu>
2022-10-14 10:35:37 +0200

    basis

---
+++ Pokemon/PokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonApp.swift
+//  Pokemon
+//
+//  Created by Nguyen Tamás on 2022. 10. 14..
+//
+
+import SwiftUI
+
+@main
+struct PokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
